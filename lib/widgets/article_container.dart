90f7e7e4f67d3de8bbc7493c03d79f0fb45401ed
ReyoKatsu <38076269+Honekatsu@users.noreply.github.com>
2024-08-20 18:13:50 +0900

    article_screen

--- lib/widgets/article_container.dart
+++ lib/widgets/article_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/article_screen.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_application_1/models/article.dart';
 
@@ -14,90 +15,99 @@ class ArticleContainer extends StatelessWidget {
         vertical: 12,
         horizontal: 16,
       ),
-      child: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 16,
-        ),
-        decoration: const BoxDecoration(
-          color: Color(0xFF55C500),
-          borderRadius: BorderRadius.all(
-            Radius.circular(32),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: ((context) => ArticleScreen(article: article)),
+            ),
+          );
+        },
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 16,
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Date
-            Text(
-              DateFormat('yyyy/MM/dd').format(article.createdAt),
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 12,
-              ),
+          decoration: const BoxDecoration(
+            color: Color(0xFF55C500),
+            borderRadius: BorderRadius.all(
+              Radius.circular(32),
             ),
-            // Title
-            Text(
-              article.title,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Date
+              Text(
+                DateFormat('yyyy/MM/dd').format(article.createdAt),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                ),
               ),
-            ),
-            // Tags
-            Text(
-              '#${article.tags.join(' #')}',
-              style: const TextStyle(
-                fontSize: 12,
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
+              // Title
+              Text(
+                article.title,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                // Favorite
-                Column(
-                  children: [
-                    const Icon(
-                      Icons.favorite,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      article.likesCount.toString(),
-                      style: const TextStyle(
-                        fontSize: 12,
+              // Tags
+              Text(
+                '#${article.tags.join(' #')}',
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  // Favorite
+                  Column(
+                    children: [
+                      const Icon(
+                        Icons.favorite,
                         color: Colors.white,
                       ),
-                    ),
-                  ],
-                ),
-                // user
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    CircleAvatar(
-                      radius: 26,
-                      backgroundImage:
-                          NetworkImage(article.user.profileImageUrl),
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      article.user.id,
-                      style: const TextStyle(
-                        fontSize: 12,
-                        color: Colors.white,
+                      Text(
+                        article.likesCount.toString(),
+                        style: const TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+                    ],
+                  ),
+                  // user
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      CircleAvatar(
+                        radius: 26,
+                        backgroundImage:
+                            NetworkImage(article.user.profileImageUrl),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        article.user.id,
+                        style: const TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
